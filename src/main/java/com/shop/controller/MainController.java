d289c1db6d3a36fd5f58d4326fbfcfd6b3697699
JungYoonshin <sju08227@naver.com>
2022-08-26 03:06:13 +0900

    수정사항

--- src/main/java/com/shop/controller/MainController.java
+++ src/main/java/com/shop/controller/MainController.java
@@ -14,4 +14,29 @@ public class MainController {
         return "main";
     }
 
+    @GetMapping(value = "/community")
+    public String write(){
+        return "communityWrite";
+    }
+
+    @GetMapping(value = "/profile")
+    public String profile(){
+        return "Profile";
+    }
+
+    @GetMapping(value = "/communityList1")
+    public String list(){
+        return "community/list1";
+    }
+
+    @GetMapping(value = "/communityList2")
+    public String list2(){
+        return "community/list2";
+    }
+
+    @GetMapping(value = "/communityList3")
+    public String list3(){
+        return "community/list3";
+    }
+
 }
